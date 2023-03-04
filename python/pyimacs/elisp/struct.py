154c73e46e745eb010dd696c3c3091b8e9f5b92c
Superjomn <yanchunwei@outlook.com>
2023-03-04 10:13:34 +0800

    up

--- python/pyimacs/elisp/struct.py
+++ python/pyimacs/elisp/struct.py
@@ -1,17 +1,17 @@
-import inspect
 import ast
-import astpretty
+import inspect
 from typing import *
+
+import astpretty
+
 
 class ClassCompiler(ast.NodeVisitor):
     def visit_ClassDef(self, node: ast.ClassDef) -> Any:
         node.body = [self.visit(b) for b in node.body]
+
 
 def struct(class__):
     source = inspect.getsource(class__)
     class_ast = ast.parse(source)
     astpretty.pprint(class_ast)
     return class__
-
-
-
