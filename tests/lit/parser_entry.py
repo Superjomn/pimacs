6bfb5712e3b995b2be26b9ae0f59687be71f1b9d
Yan Chunwei <yanchunwei@outlook.com>
2024-04-25 21:15:52 +0800

    Add FileCheck tests (#23)

---
+++ tests/lit/parser_entry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from io import StringIO
+
+import click
+
+from pimacs.lang.ir_visitor import IRPrinter
+from pimacs.lang.parser import parse
+
+
+@click.command()
+@click.option("--filename", type=str)
+@click.option("--buildir", type=bool,default=False)
+def main(filename:str, buildir:bool):
+    file = parse(filename=filename, build_ir=buildir)
+    printer = IRPrinter(StringIO())
+    printer(file)
+    output = printer.os.getvalue()
+
+    print(output)
+
+if __name__ == '__main__':
+    main()
