8d24cc83294c025b677868dac136dfd45df64594
Superjomn <yanchunwei@outlook.com>
2024-05-07 08:23:50 +0800

    avoid rule in transformer

--- pimacs/lang/parser.py
+++ pimacs/lang/parser.py
@@ -9,6 +9,7 @@ from typing import *
 import lark
 from lark import Lark, Transformer
 from lark.indenter import PythonIndenter
+from lark.lexer import Token
 
 import pimacs.lang.ir as ir
 import pimacs.lang.type as _type
@@ -52,9 +53,11 @@ class PimacsTransformer(Transformer):
         self.source = source
 
     def file_input(self, items):
+        self._force_non_rule(items)
         return items
 
     def func_def(self, items):
+        self._force_non_rule(items)
         name: str = items[0].value
         args: List[ir.ArgDecl] = safe_get(items, 1, [])
         type = safe_get(items, 2, None)
@@ -69,9 +72,11 @@ class PimacsTransformer(Transformer):
                            loc=loc)
 
     def func_args(self, items):
+        self._force_non_rule(items)
         return items
 
     def func_arg(self, items) -> ir.ArgDecl:
+        self._force_non_rule(items)
         name_token = safe_get(items, 0, None)
         name = name_token.value if name_token else None
         type_ = safe_get(items, 1, None)
@@ -81,14 +86,17 @@ class PimacsTransformer(Transformer):
 
 
     def type_base(self, items):
+        self._force_non_rule(items)
         return items[0]
 
     def type(self, items):
+        self._force_non_rule(items)
         assert len(items) == 1
         return items[0]
 
     def set_type(self, items):
         "The type of Set"
+        self._force_non_rule(items)
         assert len(items) == 1
         return _type.SetType(inner_types=items)
 
@@ -96,6 +104,7 @@ class PimacsTransformer(Transformer):
         '''
         Type of Dict.
         '''
+        self._force_non_rule(items)
         assert len(items) == 2
         return _type.DictType(key_type=items[0], value_type=items[1])
 
@@ -103,15 +112,18 @@ class PimacsTransformer(Transformer):
         '''
         Type of List.
         '''
+        self._force_non_rule(items)
         assert len(items) == 1
         return _type.ListType(inner_types=items)
 
 
     def PRIMITIVE_TYPE(self, items) -> _type.Type:
+        self._force_non_rule(items)
         # This terminal is failed, because it is covered by the custom_type
         raise NotImplementedError()
 
     def custom_type(self, items) -> _type.Type:
+        self._force_non_rule(items)
         assert len(items) == 1
         ret = _type.parse_primitive_type(items[0].value)
         if ret is not None:
@@ -119,6 +131,7 @@ class PimacsTransformer(Transformer):
         return _type.Type(type_id=_type.TypeId.CUSTOMED, name=items[0].value)
 
     def block(self, items) -> ir.Block:
+        self._force_non_rule(items)
         stmts = list(filter(lambda x: x is not None, items))
         doc_string = None
         if isinstance(items[0], ir.DocString):
@@ -129,6 +142,7 @@ class PimacsTransformer(Transformer):
 
 
     def return_stmt(self, items) -> ir.ReturnStmt:
+        self._force_non_rule(items)
         assert len(items) == 2
         loc = ir.Location(self.source, items[0].line, items[0].column)
         if items[1]:
@@ -136,13 +150,15 @@ class PimacsTransformer(Transformer):
         return ir.ReturnStmt(value=None, loc=loc)
 
     def type_spec(self, items):
+        self._force_non_rule(items)
         return items[0]
 
     def type_list(self, items):
+        self._force_non_rule(items)
         return items
 
     def func_call(self, items) -> ir.FuncCall | ir.LispFuncCall:
-        print('func_call items', items)
+        self._force_non_rule(items)
         if isinstance(items[0], ir.VarRef):
             name = items[0].name
             loc = items[0].loc
@@ -168,30 +184,40 @@ class PimacsTransformer(Transformer):
             return ir.FuncCall(func=name, args=args, loc=loc, type_spec=type_spec)
 
     def lisp_name(self, items):
+        self._force_non_rule(items)
         raise NotImplementedError()
 
     def lisp_symbol(self, items):
+        self._force_non_rule(items)
         return ir.LispVarRef(name=items[0].value, loc=ir.Location(self.source, items[0].line, items[0].column))
 
     def func_call_name(self, items) -> str:
+        self._force_non_rule(items)
         return items[0]
 
     def call_param(self, items: List[lark.Tree]) -> ir.CallParam:
+        self._force_non_rule(items)
         assert False
         assert len(items) == 1
         content = items[0][0]
         return ir.CallParam(name="", value=content, loc=content.loc)
 
+    def call_param_name(self, items) -> Token:
+        return items[0]
+
     def call_params(self, items: List[lark.Tree]) -> List[ir.CallParam]:
+        self._force_non_rule(items)
         for item in items:
             assert isinstance(item, ir.CallParam)
         return items # type: ignore
 
-    def string(self, items: List[lark.lexer.Token]):
+    def string(self, items: List[Token]):
+        self._force_non_rule(items)
         loc = ir.Location(self.source, items[0].line, items[0].column) # type: ignore
         return ir.Constant(value=items[0].value, loc=loc)
 
     def variable(self, items) -> ir.VarRef:
+        self._force_non_rule(items)
         assert len(items) == 1
         if isinstance(items[0], ir.VarRef):
             return items[0]
@@ -201,6 +227,7 @@ class PimacsTransformer(Transformer):
         return ir.VarRef(name=items[0].value, loc=ir.Location(self.source, items[0].line, items[0].column))
 
     def var_decl(self, items) -> ir.VarDecl:
+        self._force_non_rule(items)
         loc = ir.Location(self.source, items[0].line, items[0].column)
         name = items[1].value
         type = safe_get(items, 2, None)
@@ -210,6 +237,7 @@ class PimacsTransformer(Transformer):
         return node
 
     def let_decl(self, items) -> ir.VarDecl:
+        self._force_non_rule(items)
         loc = ir.Location(self.source, items[0].line, items[0].column)
         name = items[1].value
         type = safe_get(items, 2, None)
@@ -243,99 +271,121 @@ class PimacsTransformer(Transformer):
         return var
 
     def value_param(self, items):
+        self._force_non_rule(items)
         assert len(items) == 1
         return ir.CallParam(name="", value=items[0], loc=items[0].loc)
 
     def key_value_param(self, items):
-        assert isinstance(items[0], lark.Tree)
-        tree: lark.Tree = items[0]
-        assert isinstance(tree.children[0], lark.lexer.Token)
-        name:str = tree.children[0].value
+        self._force_non_rule(items)
+        assert isinstance(items[0], Token)
+        name = items[0].value
         value = items[1]
-        loc = ir.Location(self.source, tree.children[0].line, tree.children[0].column)
+        loc = ir.Location(self.source, items[0].line, items[0].column)
         return ir.CallParam(name=name, value=value, loc=loc)
 
     def expr(self, items):
+        self._force_non_rule(items)
         return items[0]
 
-    def number(self, x: List[ir.Constant]):
-        assert len(x) == 1
-        return x[0]
+    def number(self, items: List[ir.Constant]):
+        self._force_non_rule(items)
+        assert len(items) == 1
+        return items[0]
 
     def NUMBER(self, x):
+        self._force_non_rule(x)
         value = float(x) if '.' in x else int(x)
         return ir.Constant(value=value, loc=ir.Location(self.source, x.line, x.column))
 
     def atom(self, items):
+        self._force_non_rule(items)
         return items[0]
 
     def true(self, items):
+        self._force_non_rule(items)
         return ir.Constant(value=True, loc=None)
 
     def false(self, items):
+        self._force_non_rule(items)
         return ir.Constant(value=False, loc=None)
 
     def nil(self, items):
+        self._force_non_rule(items)
         return ir.Constant(value=None, loc=None)
 
     def if_stmt(self, items):
+        self._force_non_rule(items)
         loc = ir.Location(self.source, items[0].line, items[0].column)
         cond = items[1]
         then_block = items[2]
 
         elif_blocks = []
         else_block = None
-        for i in range(3, len(items)):
-            rule: lark.tree.Tree = items[i]
-            rule_kind:str = items[i].data
 
-            if rule_kind == 'else_block':
-                else_block = rule.children[1]
-            elif rule_kind == 'elif_block':
-                elif_cond = rule.children[1]
-                elif_block = rule.children[2]
-                elif_blocks.append((elif_cond, elif_block))
+        for item in items[3:]:
+            if item[0] == "elif":
+                elif_blocks.append(item[1:])
+            elif item[0] == "else":
+                else_block = item[1]
+            else:
+                raise ValueError(f"{loc}\nUnknown if block {item}")
 
         return ir.IfStmt(cond=cond, then_branch=then_block, elif_branches=elif_blocks, else_branch=else_block, loc=loc)
 
+    def elif_block(self, items):
+        return "elif", items[1], items[2] # cond, block
+
+    def else_block(self, items):
+        return "else", items[1] # block
+
     def add(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.ADD, loc=items[0].loc)
 
     def sub(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.SUB, loc=items[0].loc)
 
     def mul(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.MUL, loc=items[0].loc)
 
     def div(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.DIV, loc=items[0].loc)
 
     def le(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.LE, loc=items[0].loc)
 
     def lt(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.LT, loc=items[0].loc)
 
     def ge(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.GE, loc=items[0].loc)
 
     def gt(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.GT, loc=items[0].loc)
 
     def eq(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.EQ, loc=items[0].loc)
 
     def ne(self, items):
+        self._force_non_rule(items)
         return ir.BinaryOp(left=items[0], right=items[1], op=ir.BinaryOperator.NE, loc=items[0].loc)
 
     def decorator(self, items):
-        print('items', items)
+        self._force_non_rule(items)
         action = None
         args = []
         if isinstance(items[0], ir.FuncCall):
             action: ir.FuncCall = items[0]
             loc = action.loc
-        elif isinstance(items[0], lark.Token):
+        elif isinstance(items[0], Token):
             if items[0].value == "template":
                 loc = ir.Location(self.source, items[0].line, items[0].column)
                 action = ir.Template(items[1])
@@ -347,39 +397,47 @@ class PimacsTransformer(Transformer):
         return ir.Decorator(action=action, loc=loc)
 
     def decorated(self, items):
+        self._force_non_rule(items)
         decorators: List[ir.Decorator] = items[:-1]
         func_def: ir.FuncDecl = items[-1]
         func_def.decorators = decorators
         return func_def
 
     def dotted_name(self, items) -> ir.VarRef:
+        self._force_non_rule(items)
         return ir.VarRef(name=".".join([x.value for x in items]), loc=ir.Location(self.source, items[0].line, items[0].column))
 
     def assign_stmt(self, items):
+        self._force_non_rule(items)
         target = items[0]
         value = items[1]
         return ir.AssignStmt(target=target, value=value, loc=target.loc)
 
     def class_def(self, items):
+        self._force_non_rule(items)
         name = items[0].value
         body = items[1]
         loc = ir.Location(self.source, items[0].line, items[0].column)
         return ir.ClassDef(name=name, body=body, loc=loc)
 
     def class_body(self, items):
+        self._force_non_rule(items)
         return filter(lambda x: x is not None, items)
 
     def STRING(self, items):
+        self._force_non_rule(items)
         assert isinstance(items, str)
         return items
 
     def doc_string(self, items):
+        self._force_non_rule(items)
         token = items[0]
         assert token.value.startswith('"') and token.value.endswith('"')
         content = token.value[1:-1]
         return ir.DocString(content=content.strip(), loc=ir.Location(self.source, token.line, token.column))
 
     def select_expr(self, items):
+        self._force_non_rule(items)
         true_expr = items[0]
         cond = items[1]
         false_expr = items[2]
@@ -390,22 +448,30 @@ class PimacsTransformer(Transformer):
         return ir.SelectExpr(cond=cond, then_expr=true_expr, else_expr=false_expr, loc=cond.loc)
 
     def guard_stmt(self, items):
+        self._force_non_rule(items)
         func_call = items[0]
         body = items[1]
 
         return ir.GuardStmt(header=func_call, body=body, loc=func_call.loc)
 
     def not_cond(self, items):
+        self._force_non_rule(items)
         loc = ir.Location(self.source, items[0].line, items[0].column)
         assert isinstance(items[1], ir.Expr)
         return ir.UnaryOp(op=ir.UnaryOperator.NOT, value=items[1], loc=loc)
 
     def type_placeholders(self, items):
+        self._force_non_rule(items)
         return [_type.make_customed(name=item.value) for item in items]
 
     def type_placeholder_list(self, items):
-        print(f"type_placeholder_list: {items}")
+        self._force_non_rule(items)
         return items[0]
+
+    def _force_non_rule(self, items):
+        items = [items] if not isinstance(items, list) else items
+        trees = list(filter(lambda x: isinstance(x, lark.Tree), items))
+        assert not trees, f"Unknown rule {trees}"
 
 
 def safe_get(items, index, default):
